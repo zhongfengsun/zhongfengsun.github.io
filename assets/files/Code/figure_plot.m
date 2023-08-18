736f8668aa615bea46bbfa8f61d5859f71531345
zhongfengsun <zfsun@sdut.edu.cn>
2023-08-18 01:43:07 +0000

    删除文件 assets/files/Code/figure_plot.m

--- assets/files/Code/figure_plot.m
+++
@@ -1,7 +0,0 @@
-clear;clc;
-load suc.xls;
-plot(suc(:,1),suc(:,2),'ks-',suc(:,1),suc(:,3),'ro-',suc(:,1),suc(:,4),'gd-',suc(:,1),suc(:,5),'b^-','LineWidth',1,'MarkerSize',6),
-ylabel('Succcess  frequencies  for  recovery'),xlabel('Sparsity  level'),
-legend('HBIHT','HBHTP','IHT','HTP')
-%axis([0, 300,0,100]),%set(gca,'XTick',0:30:300)
-
